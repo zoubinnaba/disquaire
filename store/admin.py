35f2463db89a6949d30b03bf9cf862319777183c
zoubinnaba <emmanuelzoubinnaba@gmail.com>
2021-04-16 12:37:17 +0000

    custumisation de la page d'adminstration

--- store/admin.py
+++ store/admin.py
@@ -1,8 +1,71 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+from django.urls import reverse
+from django.contrib.contenttypes.models import ContentType
 
 from store.models import Album, Artiste, Contact, Booking
 
-admin.site.register(Album)
-admin.site.register(Artiste)
-admin.site.register(Contact)
-admin.site.register(Booking)
+
+class AdminURLMixin(object):
+
+    def get_admin_url(self, obj):
+        content_type = ContentType.objects.get_for_model(obj.__class__)
+        return reverse("admin:store_%s_change" % (content_type.model), args=(obj.id,))
+
+
+class BookingInline(admin.TabularInline, AdminURLMixin):
+    model = Booking
+    readonly_fields = ['created_at', 'contacted', 'album_link']
+    fieldsets = [
+        (None, {'fields': ['album_link', 'contacted']})
+    ]
+    extra = 0
+    verbose_name = "Réservation"
+    verbose_name_plural = "Réservations"
+
+    def has_add_permission(self, request):
+        return False
+
+    def album_link(self, booking):
+        url = self.get_admin_url(booking.album)
+        return mark_safe(f"<a href='{url}'>{booking.album.title}</a>")
+
+
+class AlbumArtisteInline(admin.TabularInline):
+    model = Album.artistes.through
+    extra = 1
+    verbose_name = "Disque"
+    verbose_name_plural = "Disques"
+
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    inlines = [BookingInline, ]
+
+
+@admin.register(Artiste)
+class ArtisteAdmin(admin.ModelAdmin):
+    inlines = [AlbumArtisteInline, ]
+
+
+@admin.register(Album)
+class AlbumAdmin(admin.ModelAdmin):
+    search_fields = ['reference', 'title']
+
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin, AdminURLMixin):
+    readonly_fields = ['created_at', 'contact_link', 'album_link']
+    fields = ['created_at', 'album_link', 'contacted']
+    list_filter = ['created_at', 'contacted']
+
+    def has_add_permission(self, request):
+        return False
+
+    def contact_link(self, booking):
+        url = self.get_admin_url(booking.contact)
+        return mark_safe(f"<a href='{url}'>{booking.contact.name}</a>")
+
+    def album_link(self, booking):
+        url = self.get_admin_url(booking.album)
+        return mark_safe(f"<a href='{url}'>{booking.album.title}</a>")
--- store/models.py
+++ store/models.py
@@ -2,37 +2,49 @@ from django.db import models
 
 
 class Artiste(models.Model):
-    name = models.CharField(max_length=200, unique=True)
+    name = models.CharField('Nom', max_length=200, unique=True)
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        verbose_name = "artiste"
 
 
 class Contact(models.Model):
-    email = models.EmailField(max_length=100)
-    name = models.CharField(max_length=200)
+    email = models.EmailField('Email', max_length=100)
+    name = models.CharField('Nom', max_length=200)
 
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name = "prospect"
+
 
 class Album(models.Model):
-    reference = models.IntegerField(null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    available = models.BooleanField(default=True)
-    title = models.CharField(max_length=200)
-    picture = models.URLField()
+    reference = models.IntegerField('Référence', null=True)
+    created_at = models.DateTimeField('Date de creation', auto_now_add=True)
+    available = models.BooleanField('Disponible', default=True)
+    title = models.CharField('Titre', max_length=200)
+    picture = models.URLField("Url de l'image")
     artistes = models.ManyToManyField(Artiste, related_name='albums', blank=True)
 
     def __str__(self):
         return self.title
 
+    class Meta:
+        verbose_name = "disque"
+
 
 class Booking(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
-    contacted = models.BooleanField(default=False)
+    created_at = models.DateTimeField("Date d'envoi", auto_now_add=True)
+    contacted = models.BooleanField('Demande traitée?', default=False)
     contact = models.ForeignKey(Contact, on_delete=models.CASCADE)
     album = models.OneToOneField(Album, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.contact.name
+
+    class Meta:
+        verbose_name = "réservation"
