94732276f3780f0b4dc7f1de83d9e1f72da0f708
zoubinnaba <emmanuelzoubinnaba@gmail.com>
2021-04-16 15:23:52 +0000

    configuration du serveur

--- disq/settings.py
+++ disq/settings.py
@@ -17,7 +17,7 @@ if os.environ.get('ENV') == 'PRODUCTION':
 else:
     DEBUG = True
 
-ALLOWED_HOSTS = ['disqapp.herokuapp.com', ' http://0.0.0.0:19109']
+ALLOWED_HOSTS = ['0.0.0.0', 'https://disqapp.herokuapp.com/']
 
 
 # Application definition
